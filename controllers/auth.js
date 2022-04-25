9cd15422c277525776d9cc9cd1c63bfc92e014cc
Alvaro <alvaroruizcatoira@gmail.com>
2022-04-25 15:55:08 -0400

    Google Sing addeed

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,9 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario =  require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
+
 
 
 
@@ -57,6 +60,60 @@ const login = async ( req, res = response ) => {
 
 }
 
+const googleSignIn  = async ( req, res = response ) => {
+
+    const { id_token } = req.body;
+
+    try {
+        
+        const { correo, nombre, img, rol } = await googleVerify( id_token );
+        
+        
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if( !usuario ){
+            //Tengo que crearlo
+            const data = { 
+                nombre, 
+                correo,
+                rol: 'USER_ROLE',
+                password: ':p',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //si el usuario en DB
+        if( !usuario.estado ){
+            return res.status( 401 ).json({ 
+                msg:'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            token,
+            usuario
+        })
+
+    } catch (error) {
+        res.status( 400 ).json({
+            ok: false, 
+            msg: 'El token no se pudo verificar'
+        })
+
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn 
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -11,5 +11,10 @@ router.post('/login',[
     check('password', 'El contrasña es obligatorio').not().isEmpty(),
     validarCampos
 ], login );
+
+router.post('/google',[
+    check('id_token', 'id_token debe ser necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn );
 
 module.exports = router;
